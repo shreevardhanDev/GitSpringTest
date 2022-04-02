e78d2a6a86b90052fdfc6e76bc51f9206ed2f58c
shreevardhanDev <vardhansasuke@gmail.com>
2022-04-02 14:25:41 +0530

    plannig to merge to master branch

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Working Feature version");
+		System.out.println("Working New Thing to merge into master branch version");
 	}
 
 }
